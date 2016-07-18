4da07f44024c6e787a451628e0d5a90e2f8a4c99
394356808 <394356808@qq.com>
2016-07-18 10:36:36 +0800

    af

---
+++ src/com/redis/Main.java
@@ -0,0 +1,13 @@
+package com.redis;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    TestRedis test=new TestRedis();
+	    //在这可以调用TestRedis中那几个方法测试下
+	    test.testRedisPool();
+	}
+
+}
